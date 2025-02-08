b966893b913718f4cee44080a6d06630cfded114
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2025-02-08 18:56:39 +0900

    Update cbindgen, fix configs

--- generate_header.sh
+++ generate_header.sh
@@ -1,2 +1,2 @@
 # Must be run in the crate's root directory
-cbindgen --config cbindgen.toml --parse-dependencies --crate shogi_legality_lite --output include/shogi_legality_lite.h $@
+cbindgen --config cbindgen.toml --crate shogi_legality_lite --output include/shogi_legality_lite.h $@
